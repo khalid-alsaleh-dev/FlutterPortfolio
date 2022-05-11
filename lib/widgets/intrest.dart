9ffeebeb6d32d49466ebe2991e22e9c30291c050
Akash Jaiswal <33419526+jaiakash@users.noreply.github.com>
2022-05-11 12:29:49 +0530

    typo: interest

--- lib/widgets/intrest.dart
+++ lib/widgets/intrest.dart
@@ -25,7 +25,7 @@ class Intrest extends StatelessWidget {
                   ? CustomColors.primary
                   : textColor)),
       child: Center(
-          child: Text('.' + intrest,
+          child: Text('.' + interest,
               style: GoogleFonts.getFont('Delius',
                   color: textColor, fontSize: 13))),
     );
